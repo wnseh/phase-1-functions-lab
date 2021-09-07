2a378d58edcd197922c1c52540b77228d0cc5c43
Young Jun Do <youngjundoe@gmail.com>
2021-09-07 16:54:26 -0400

    commit tree 648d2858ccd5317a2149d83e8dd15693e2037ca9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(42 - street);
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start-end) * 264;
+}
+
+function distanceFromHqInFeet(street){
+    return distanceTravelledInFeet(0,distanceFromHqInBlocks(street));
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceTraveled = distanceTravelledInFeet(start, destination);
+    if( distanceTraveled <=400){
+        return 0;}
+    else if(distanceTraveled > 2500){
+        return "cannot travel that far";}
+    else if (distanceTraveled > 2000){
+        return 25;}
+    else{
+        return 0.02*(distanceTraveled-400);}
+}
+
